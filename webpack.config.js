c16e11b88fd7fe8deede5bc4ce4cfd22b49198f9
dubaua <dubaua@gmail.com>
2019-09-23 16:07:44 +0500

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,68 @@
-'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const I18nPlugin = require('i18n-webpack-plugin');
+const languages = {
+  en: require('./i18n/en.json'),
+  ru: require('./i18n/ru.json'),
+};
 
-const environment = (process.env.NODE_ENV || 'development').trim();
+const isDev = process.env.NODE_ENV !== 'production';
 
-if (environment === 'development') {
-  module.exports = require('./config/webpack.config.dev');
-} else {
-  module.exports = require('./config/webpack.config.prod');
-}
+module.exports = Object.keys(languages).map(function(language) {
+  return {
+    name: language,
+    entry: {
+      main: path.resolve(__dirname, 'example/main.js'),
+    },
+    output: {
+      path: __dirname + '/docs',
+      filename: 'main.js',
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/,
+          exclude: /svg[\/\\]/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'images/[name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './example/index.html',
+        filename: language === 'en' ? 'index.html' : language + '.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDev ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+      }),
+      new I18nPlugin(languages[language]),
+    ],
+  };
+});
