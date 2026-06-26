cff8193f449c32bd70b67da53f6a3482be0da784
Vladimir Lysov <dubaua@gmail.com>
2026-06-26 12:05:00 +0500

    expose styles for external renderer

--- src/immerser.ts
+++ src/immerser.ts
@@ -1106,4 +1106,4 @@ export type {
   SolidClassnames,
   SolidClassnamesByLayerId,
 };
-export { EventNames };
+export { EventNames, CroppedFullAbsoluteStyles, InteractiveStyles, NotInteractiveStyles };
--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,4 @@
+/** @public Style used for masks and mask inners, exposed to use with external rendered */
 export const CroppedFullAbsoluteStyles: Record<string, string> = {
   position: 'absolute',
   top: '0',
@@ -7,11 +8,13 @@ export const CroppedFullAbsoluteStyles: Record<string, string> = {
   overflow: 'hidden',
 };
 
+/** @public Style for immerser root, to make in click transparent, exposed to use with external rendered */
 export const NotInteractiveStyles: Record<string, string> = {
   pointerEvents: 'none',
   touchAction: 'none',
 };
 
+/** @public Style for immerser solids, to make them clickable again inside not clickable immerser root, exposed to use with external rendered */
 export const InteractiveStyles: Record<string, string> = {
   pointerEvents: 'all',
   touchAction: 'auto',
